06e77c5d6227e15f77724354f5e65f5d110ce1f0
usmaniyya <usmanyahaya6967@gmail.com>
2023-11-17 08:38:19 -0800

    commit

--- pages/patient.jsx
+++ pages/patient.jsx
@@ -7,8 +7,8 @@ const patient = () => {
       <form className='w-[400px]'>
       <h2 className="my-6 text-2xl font-bold text-dark/75 text-center">Patient</h2>
         <div className="flex flex-col gap-1">
-          <label htmlFor="phone-number" id="number" placeholder="+2348***********">Phone Number</label>
-          <input type="text" className="p-2 text-dark border border-solid border-indigo-500 outline-none rounded"/>
+          <label htmlFor="phone-number">Phone Number</label>
+          <input type="text" id="number" placeholder="+2348***********" className="p-2 text-dark border border-solid border-indigo-500 outline-none rounded"/>
         </div>
         <button type="submit" className='w-full mt-2 p-2 bg-indigo-600 hover:bg-indigo-400 text-white text-center text-lg boder-none'>Send OTP</button>
         <div className="flex flex-col gap-1">
